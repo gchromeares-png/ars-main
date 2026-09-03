c7066dcb3bd4556c755f0ae7cef7ded28eca808c
gchromeares-png <gchrome.ares@gmail.com>
2026-09-03 12:15:59 +0200

    Register runtime UI component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
+import { RuntimeControlComponent } from "./runtime-control/runtime-control.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, RuntimeControlComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
