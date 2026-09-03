10eeb359e3d49468ac59fbc1c2b1413855f59f93
gchromeares-png <gchrome.ares@gmail.com>
2026-09-03 23:47:31 +0200

    Fix semantic field embedding test ambiguity

--- tests/semantic-field-resolver.test.ts
+++ tests/semantic-field-resolver.test.ts
@@ -31,7 +31,7 @@ class ConceptEmbeddingProvider implements SemanticEmbeddingProvider {
       [/mail|e-mail/, 0],
       [/given|first name|vorname|rufname/, 1],
       [/family|surname|nachname|familienname/, 2],
-      [/street|straße|hausnummer|anschrift/, 3],
+      [/street|straße|hausnummer|straßenanschrift/, 3],
       [/secondary|apartment|adresszusatz|zusatz/, 4],
       [/city|town|locality|stadt|ort|gemeinde|lieferort/, 5],
       [/postal|zip|postleitzahl|zustellcode|postgebiet/, 6],
