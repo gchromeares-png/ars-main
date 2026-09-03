04253db4b71ba744e9f45912df5a82b4b8f76bfb
gchromeares-png <gchrome.ares@gmail.com>
2026-09-03 21:57:53 +0200

    Update types.ts

--- src/challenges/types.ts
+++ src/challenges/types.ts
@@ -5,6 +5,9 @@ export type LiveChallengeType =
   | "shopify-checkpoint"
   | "shopify-queue"
   | "generic-interstitial"
+    "queue-it"
+    "datadome"
+    "waiting-room"
   | "unknown";
 
 export interface LiveChallengeDetection {
