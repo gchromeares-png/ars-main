fe1c23dbb86ddf01df56f5769284791810dcab74
gchromeares-png <gchrome.ares@gmail.com>
2026-09-04 08:09:35 +0200

    Keep API key status inside runtime control

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,9 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { DropSetupsComponent } from "./drop-setups/drop-setups.component";
 import { RuntimeControlComponent } from "./runtime-control/runtime-control.component";
-import { ApiKeyStatusComponent } from "./api-key-status/api-key-status.component";
 
 @NgModule({
-  declarations: [AppComponent, RuntimeControlComponent, DropSetupsComponent, ApiKeyStatusComponent],
+  declarations: [AppComponent, RuntimeControlComponent, DropSetupsComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
