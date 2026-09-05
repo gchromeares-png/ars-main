90159d7ce986e3fca9f7fd387aaad7113c25cb0b
gchromeares-png <gchrome.ares@gmail.com>
2026-09-05 09:05:49 +0200

    wip: remove legacy browser worker alias

--- src/browser-worker/patchright-browser-worker.ts
+++
@@ -1,8 +0,0 @@
-import { SeleniumBaseBrowserWorker } from "./seleniumbase-browser-worker";
-
-/**
- * Backwards-compatible class name for existing executor imports.
- * Runtime implementation is SeleniumBase Pure CDP; Patchright is no longer
- * instantiated or required by this worker.
- */
-export class PatchrightBrowserWorker extends SeleniumBaseBrowserWorker {}
